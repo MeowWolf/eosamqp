c8d9e9832050bed1bb0d2f613d480a7005627f14
Amo DelBello <2342804+amodelbello@users.noreply.github.com>
2021-03-13 09:19:09 -0700

    PTFM-902 Add back dep inject for Dial()

--- eosamqp.go
+++ eosamqp.go
@@ -95,8 +95,7 @@ func New(d *deps) Amqp {
 
 // Connect creates a new amqp connection
 func (a *Amqp) Connect(brokerURL string) (*amqp.Connection, error) {
-	// conn, err := a.dial(brokerURL)
-	conn, err := amqp.Dial(brokerURL)
+	conn, err := a.dial(brokerURL)
 	if err != nil {
 		a.conn = nil
 		a.logError("failed to connect to RabbitMQ: %s", err)
